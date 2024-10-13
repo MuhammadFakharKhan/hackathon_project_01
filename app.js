5221938caa65065a1071689f2e0470798d75d7fe
Muhammad Fakhar Khan <159140926+MuhammadFakharKhan@users.noreply.github.com>
2024-10-13 20:06:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggle-skills');
+    var skillsSection = document.getElementById('skills');
+    toggleButton === null || toggleButton === void 0 ? void 0 : toggleButton.addEventListener('click', function () {
+        if (skillsSection) {
+            skillsSection.style.display = skillsSection.style.display === 'block' ? 'none' : 'block';
+        }
+    });
+});
